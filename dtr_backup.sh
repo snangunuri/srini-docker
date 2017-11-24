b3d96da083a5397e884c165ee122f40a8288a520
root <root@ip-172-31-14-18.ec2.internal>
2017-11-24 18:02:21 +0000

    Adding changes to the files

--- dtr_backup.sh
+++ dtr_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#set -x
 DTR_VERSION=2.4.0
 UCP_URL=https://54.175.154.26:443
 UCP_USERNAME=admin
@@ -7,23 +8,21 @@ echo $REPLICA_ID
 TODAY=`date +%Y%m%d`
 HOSTNAME=`hostname`
 EMAIL_RECIPIENTS='srinivasa.nangunuri@aurotechcorp.com'
+EMAIL_SUBJECT=''
 REPLICA_ID=`docker ps -lf name='^/dtr' --format '{{.Names}}' | cut -d- -f3`
-email_function () {
-  echo "checking the email $1"
-  echo "example body" > /tmp/email_body.txt
-  mail -s "example subject" $EMAIL_RECIPIENTS /tmp/email_body.txt
-}
-#email_function srini
+#email_function subject my_file
 if [ ! -z "$REPLICA_ID" ];then
   docker run -i --rm docker/dtr:$DTR_VERSION backup \
      --ucp-url $UCP_URL --ucp-insecure-tls \
      --ucp-username $UCP_USERNAME --ucp-password $UCP_PASSWORD \
-     --existing-replica-id $REPLICA_ID > /tmp/srini-docker/$HOSTNAME-dtr-backup-$TODAY.tar
+     --xxxexisting-replica-id $REPLICA_ID > /tmp/srini-docker/$HOSTNAME-dtr-backup-$TODAY.tar 2> error_output_file
   exit_status=$?
   if [ $exit_status -ne 0 ]; then
-    echo "Exist status $exit_status is not equal to 0"
+    EMAIL_SUBJECT="Exist status $exit_status is not equal to 0 from the backup script"
+    echo $EMAIL_SUBJECT
+    #/bin/mail -s $EMAIL_SUBJECT $EMAIL_RECIPIENTS  error_output_file
   fi
 else
-  echo "REPLICA_ID not found"   
+  EMAIL_SUBJECT="REPLICA_ID not found"   
 fi
 
--- log_rotate.sh
+++ log_rotate.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
  
 usage() { 
+    echo "*******************************************************************************************************"
     echo "Usage: $0 [-c <num of days to keep the backup files>] [-d <directory for the files to rotate>]" 1>&2
-    echo "Example: $0 -c 10 -d /tmp/dtr-backup-dir"; exit 1
+    echo "Example: $0 -c 10 -d /tmp/dtr-backup-dir"
+    echo "*******************************************************************************************************"
+    exit 1
         }
 
 while getopts ":c:d:" opt; do
   case $opt in 
     c)
-       echo "$OPTARG was triggered"
        count=$OPTARG
        ;;
     d) 
-       echo "directory is: $OPTARG"
        dtr_backup_dir=$OPTARG
        ;;
     *) 
@@ -27,13 +28,14 @@ if [ -z "${count}" ] || [ -z "${dtr_backup_dir}" ]; then
    usage
 fi
 
-if ls $dtr_backup_dir/*.tar 1> /dev/null 2>&1; then
-  find $dtr_backup_dir -type f -name \*\.tar -mtime +$count -exec rm {} \;
-  if [ $? -eq 0 ]; then
-    echo "Log rotation completed successfully"
-  else
-    echo "Log rotation failed, pleaes check"
+#if ls $dtr_backup_dir/*.tar 1> /dev/null 2>&1; then
+if ls $dtr_backup_dir/*-dtr-backup-*.tar 1> /dev/null 2>&1; then
+  find $dtr_backup_dir -type f -name \*-dtr-backup-\*\.tar -mtime +$count -exec rm {} \; 2> $dtr_backup_dir/error_output_file
+  if [ $? -ne 0 ]; then
+    EMAIL_SUBJECT="Log rotation failed"
+    /bin/mail -s $EMAIL_SUBJECT $EMAIL_RECIPIENTS $dtr_backup_dir/error_output_file
   fi
 else
-  echo "There are no files found"
+  EMAIL_SUBJECT= "There are no dtr-backup files found in the $dtr_backup_dir directory"
+  /bin/mail -s $EMAIL_SUBJECT $EMAIL_RECIPIENTS 
 fi
